6155b2215f51d80f77b859fb90ae1b0c2be154a5
il <ilmak@o2.pl>
2020-05-03 08:37:58 +0200

    zmienilem komunikaty i sposob sprawdzania danych w poleceniu SQLa ( sorawdzam od konca czyli od ostatniej danej)

--- src/Main.java
+++ src/Main.java
@@ -55,42 +55,42 @@ public class Main {
 
 			ResultSet tables = dbm.getTables(null, null, "PRZEDMIOT", null);
 			if (tables.next()) {
-				System.out.println("Table subject exist in database.");
+				System.out.println("Tabela PRZEDMIOT ju¿ istnieje w bazie danych.");
 			} else {
 				System.out.println(
-						"Create table subject in database.  execute: " + statement.execute(sqlCreateTableSubject));
+						"Tworzenie tabeli PRZEDMIOT w bazie danych. execute: " + statement.execute(sqlCreateTableSubject));
 			}
 
 			tables = dbm.getTables(null, null, "NAUCZYCIEL", null);
 			if (tables.next()) {
-				System.out.println("Table teacher exist in database.");
+				System.out.println("Tabela NAUCZYCIEL ju¿ istnieje w bazie danych.");
 			} else {
 				System.out.println(
-						"Create table teacher in database. execute: " + statement.execute(sqlCreateTableTeacher));
+						"Tworzenie tabeli NAUCZYCIEL w bazie danych. execute: " + statement.execute(sqlCreateTableTeacher));
 			}
 
 			tables = dbm.getTables(null, null, "OCENA", null);
 			if (tables.next()) {
-				System.out.println("Table rating exist in database.");
+				System.out.println("Tabela OCENA ju¿ istnieje w bazie danych.");
 			} else {
 				System.out.println(
-						"Create table rating in database. execute: " + statement.execute(sqlCreateTableRating));
+						"Tworzenie tabeli OCENA w bazie danych. execute: " + statement.execute(sqlCreateTableRating));
 			}
 
 			tables = dbm.getTables(null, null, "UCZEN", null);
 			if (tables.next()) {
-				System.out.println("Table student exist in database.");
+				System.out.println("Tabela UCZEN ju¿ istnieje w bazie danych.");
 			} else {
 				System.out.println(
-						"Create table student in database. execute: " + statement.execute(sqlCreateTableStudent));
+						"Tworzenie tabeli UCZEN w bazie danych. execute: " + statement.execute(sqlCreateTableStudent));
 			}
 
 			tables = dbm.getTables(null, null, "OCENIANIE", null);
 			if (tables.next()) {
-				System.out.println("Table evaluation exist in database.");
+				System.out.println("Tabela OCENIANIE ju¿ istnieje w bazie danych.");
 			} else {
 				System.out.println(
-						"Create table evaluation in database. execute: " + statement.execute(sqlCreateTableEvaluation));
+						"Tworzenie tabeli OCENIANIE w bazie danych. execute: " + statement.execute(sqlCreateTableEvaluation));
 			}
 
 			String sqlTestDataForSubject1 = "INSERT INTO przedmiot (idp, nazwa_przedmiotu) VALUES (1, 'Matematyka')";
@@ -99,7 +99,7 @@ public class Main {
 			String sqlTestDataForTeacher1 = "INSERT INTO nauczyciel (idn, nazwisko_nauczyciela, imie_nauczyciela) VALUES (1, 'Kowalski', 'Adam')";
 			String sqlTestDataForTeacher2 = "INSERT INTO nauczyciel (idn, nazwisko_nauczyciela, imie_nauczyciela) VALUES (5, 'Bednarska', 'Ewa')";
 
-			String sqlTestDataForRating1 = "INSERT INTO ocena (ido, wartosc_opisowa, wartosc_numeryczna) VALUES (1, 'niedostateczny', 1.5)";
+			String sqlTestDataForRating1 = "INSERT INTO ocena (ido, wartosc_opisowa, wartosc_numeryczna) VALUES (1, 'niedostateczny i pó³', 1.5)";
 			String sqlTestDataForRating2 = "INSERT INTO ocena (ido, wartosc_opisowa, wartosc_numeryczna) VALUES (5, 'dostateczny', 3)";
 
 			String sqlTestDataForStudent1 = "INSERT INTO uczen (idu, nazwisko_ucznia, imie_ucznia) VALUES (1, 'Wiœniewski', 'Karol')";
@@ -107,41 +107,41 @@ public class Main {
 
 			ResultSet rs = statement.executeQuery("SELECT * FROM przedmiot");
 			if (rs.next()) {
-				System.out.println("Table subject have data in database.");
+				System.out.println("Tabela PRZEDMIOT zawiera dane.");
 			} else {
-				System.out.println("Insert example data to table subject 1. execute:"
+				System.out.println("Wstawianie testowych danych do tabeli PRZEDMIOT 1. execute:"
 						+ statement.executeUpdate(sqlTestDataForSubject1));
-				System.out.println("Insert example data to table subject 2. execute:"
+				System.out.println("Wstawianie testowych danych do tabeli PRZEDMIOT 2. execute:"
 						+ statement.executeUpdate(sqlTestDataForSubject2));
 			}
 
 			rs = statement.executeQuery("SELECT * FROM nauczyciel");
 			if (rs.next()) {
-				System.out.println("Table teacher have data in database.");
+				System.out.println("Tabela NAUCZYCIEL zawiera dane.");
 			} else {
-				System.out.println("Insert example data to table teacher 1. execute:"
+				System.out.println("Wstawianie testowych danych do tabeli NAUCZYCIEL 1. execute:"
 						+ statement.executeUpdate(sqlTestDataForTeacher1));
-				System.out.println("Insert example data to table teacher 2. execute:"
+				System.out.println("Wstawianie testowych danych do tabeli NAUCZYCIEL 2. execute:"
 						+ statement.executeUpdate(sqlTestDataForTeacher2));
 			}
 
 			rs = statement.executeQuery("SELECT * FROM ocena");
 			if (rs.next()) {
-				System.out.println("Table rating have data in database.");
+				System.out.println("Tabela OCENA zawiera dane.");
 			} else {
-				System.out.println("Insert example data to table rating 1. execute:"
+				System.out.println("Wstawianie testowych danych do tabeli OCENA 1. execute:"
 						+ statement.executeUpdate(sqlTestDataForRating1));
-				System.out.println("Insert example data to table rating 2. execute:"
+				System.out.println("Wstawianie testowych danych do tabeli OCENA 2. execute:"
 						+ statement.executeUpdate(sqlTestDataForRating2));
 			}
 
 			rs = statement.executeQuery("SELECT * FROM uczen");
 			if (rs.next()) {
-				System.out.println("Table student have data in database.");
+				System.out.println("Tabela UCZEN zawiera dane..");
 			} else {
-				System.out.println("Insert example data to table student 1. execute:"
+				System.out.println("Wstawianie testowych danych do tabeli UCZEN 1. execute:"
 						+ statement.executeUpdate(sqlTestDataForStudent1));
-				System.out.println("Insert example data to table student 2. execute:"
+				System.out.println("Wstawianie testowych danych do tabeli UCZEN 2. execute:"
 						+ statement.executeUpdate(sqlTestDataForStudent2));
 			}
 
@@ -172,7 +172,6 @@ public class Main {
 
 			System.out.println("Wype³nianie tabeli ocenianie");
 			System.out.println(" ");
-//			System.out.println("Jeœli chcesz przerwaæ program podaj 0");
 
 			boolean rating = true;
 
@@ -180,25 +179,8 @@ public class Main {
 				System.out.println("Jeœli chcesz przerwaæ program podaj 0");
 				System.out.println("Podaj id nauczyciela:");
 
-//				if (scanner.hasNextInt()) {
-//					inputTeacherId = scanner.nextInt();
-//					if (checkIfIdExistInDataBase("NAUCZYCIEL", "idn", inputTeacherId, connection)) {
-//						preparedStatement.setInt(1, inputTeacherId);
-//					} else {
-//						System.out.println("Nie znaleziono nauczyciela o podanym ID w bazie danych");
-//						//scanner.close();
-//						break;
-//					}
-//				} else {
-//					System.out.println("B³êdne dane wejœciowe. Nie podano wartosci INT");
-//					//scanner.close();
-//					break;
-//				}
-
 				if (scanner.hasNext()) {
-
 					inputTeacherId = scanner.next();
-					;
 					if (inputTeacherId.contentEquals("0")) {
 						rating = false;
 						scanner.close();
@@ -209,20 +191,6 @@ public class Main {
 				System.out.println("Jeœli chcesz przerwaæ program podaj 0");
 				System.out.println("Podaj id ucznia:");
 
-//				if (scanner.hasNextInt()) {
-//					inputStudentId = scanner.nextInt();
-//					if (checkIfIdExistInDataBase("UCZEN", "idu", inputStudentId, connection)) {
-//						preparedStatement.setInt(2, inputStudentId);
-//					} else {
-//						System.out.println("Nie znaleziono studenta o podanym ID w bazie danych");
-//						//scanner.close();
-//						break;
-//					}
-//				} else {
-//					System.out.println("B³êdne dane wejœciowe. Nie podano wartosci INT");
-//					//scanner.close();
-//					break;
-//				}
 				if (scanner.hasNext()) {
 					inputStudentId = scanner.next();
 					if (inputStudentId.contentEquals("0")) {
@@ -236,21 +204,6 @@ public class Main {
 				System.out.println("Jeœli chcesz przerwaæ program podaj 0");
 				System.out.println("Podaj id przedmiotu:");
 
-//				if (scanner.hasNextInt()) {
-//					inputSubjectId = scanner.nextInt();
-//					if (checkIfIdExistInDataBase("PRZEDMIOT", "idp", inputSubjectId, connection)) {
-//						preparedStatement.setInt(3, inputSubjectId);
-//					} else {
-//						System.out.println("Nie znaleziono przedmiotu o podanym ID w bazie danych");
-//						//scanner.close();
-//						break;
-//					}
-//				} else {
-//					System.out.println("B³êdne dane wejœciowe. Nie podano wartosci INT");
-//					//scanner.close();
-//					break;
-//				}
-
 				if (scanner.hasNext()) {
 					inputSubjectId = scanner.next();
 					if (inputSubjectId.contentEquals("0")) {
@@ -262,21 +215,6 @@ public class Main {
 				}
 				System.out.println("Jeœli chcesz przerwaæ program podaj 0");
 				System.out.println("Podaj id oceny:");
-
-//				if (scanner.hasNextInt()) {
-//					inputRatingId = scanner.nextInt();
-//					if (checkIfIdExistInDataBase("OCENA", "ido", inputRatingId, connection)) {
-//						preparedStatement.setInt(4, inputRatingId);
-//					} else {
-//						System.out.println("Nie znaleziono oceny o podanym ID w bazie danych");
-//						//scanner.close();
-//						break;
-//					}
-//				} else {
-//					System.out.println("B³êdne dane wejœciowe. Nie podano wartosci INT");
-//					//scanner.close();
-//					break;
-//				}
 
 				if (scanner.hasNext()) {
 					inputRatingId = scanner.next();
@@ -291,27 +229,6 @@ public class Main {
 				System.out.println("Jeœli chcesz przerwaæ program podaj 0");
 				System.out.println("Podaj typ oceny: „C” –cz¹stkowa, „S” –semestralna");
 
-//				if (scanner.hasNext()) {
-//					inputRatingTypeId = scanner.next().toUpperCase();
-//					if (inputRatingTypeId.equals("C")) {
-//						preparedStatement.setString(5, "C");
-//					} else if (inputRatingTypeId.equals("S")) {
-//						if (checkIfSRattingExistInDataBase(inputTeacherId, inputStudentId, inputSubjectId,
-//								connection)) {
-//							System.out.println("B³¹d! Ocena semestralna zosta³a ju¿ wystawiona");
-//							//scanner.close();
-//							break;
-//						} else {
-//							preparedStatement.setString(5, "S");
-//						}
-//					} else {
-//						System.out.println(
-//								"B³êdne dane wejœciowe. Podano: " + inputRatingTypeId + " Nie podano wartosci C lub S");
-//						//scanner.close();
-//						break;
-//					}
-//				}
-
 				if (scanner.hasNext()) {
 					inputRatingTypeId = scanner.next().toUpperCase();
 					if (inputRatingTypeId.equals("C")) {
@@ -325,7 +242,8 @@ public class Main {
 					}
 				}
 
-				// TODO
+				scanner.close();
+
 				if (checkIfIdExistInDataBase("OCENA", "ido", inputRatingId, connection)) {
 					if (checkIfIdExistInDataBase("PRZEDMIOT", "idp", inputSubjectId, connection)) {
 						if (checkIfIdExistInDataBase("UCZEN", "idu", inputStudentId, connection)) {
@@ -333,21 +251,25 @@ public class Main {
 								System.out.println("Insert data to table evaluation. execute:"
 										+ preparedStatement.executeUpdate());
 							} else {
-								System.out.println("zly klucz 2 idn");
+								System.out.println("Error report -\n" +
+												"ORA-02291: naruszono wiêzy spójnoœci (idn) - nie znaleziono klucza nadrzêdnego"
+										);
 							}
 						} else {
-							System.out.println("zly klucz 2 idu");
+							System.out.println("Error report -\n" +
+									"ORA-02291: naruszono wiêzy spójnoœci (idu) - nie znaleziono klucza nadrzêdnego");
 						}
 					} else {
-						System.out.println("zly klucz 3 idp");
+						System.out.println("Error report -\n" +
+								"ORA-02291: naruszono wiêzy spójnoœci (idp) - nie znaleziono klucza nadrzêdnego");
 					}
 				} else {
-					System.out.println("zly klucz 4 ido");
+					System.out.println("Error report -\n" +
+							"ORA-02291: naruszono wiêzy spójnoœci (ido) - nie znaleziono klucza nadrzêdnego");
 				}
 
-//				System.out.println("Insert data to table evaluation. execute:" + preparedStatement.executeUpdate());
-			} // koniec rating
-
+			}
+			//TODO
 			connection.close();
 		} catch (SQLException e) {
 
@@ -356,21 +278,11 @@ public class Main {
 			return;
 		} finally {
 			//TODO nie dzial;
-			//connection.close();
+//			connection.close();
 		} 
 		
 		System.out.println("Koniec programu!");
 	}
-
-//	private static boolean checkIfSRattingExistInDataBase(int inputTeacherId, int inputStudentId, int inputSubjectId,
-//			Connection connection) throws SQLException {
-//		String sql = "SELECT * FROM ocenianie WHERE idn =" + inputTeacherId + "AND idu=" + inputStudentId + "AND idp="
-//				+ inputSubjectId + "AND rodzaj_oceny = 'S'";
-//
-//		Statement s = connection.createStatement();
-//		ResultSet rs = s.executeQuery(sql);
-//		return rs.next();
-//	}
 
 	private static boolean checkIfIdExistInDataBase(String table, String id, String inputTeacherId,
 			Connection connection) throws SQLException {
@@ -381,5 +293,4 @@ public class Main {
 		ResultSet rs = s.executeQuery(sql);
 		return rs.next();
 	}
-
 }
